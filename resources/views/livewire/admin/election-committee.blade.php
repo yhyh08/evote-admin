a8a5f1a516e69cbbb4d250f8b930151133a8351f
yihhuei06 <yihhuei@marslab.com.my>
2024-12-06 01:16:22 +0800

    add election

--- resources/views/livewire/admin/election-committee.blade.php
+++ resources/views/livewire/admin/election-committee.blade.php
@@ -1,33 +1,68 @@
-@extends('layouts.app')
-
-@section('content')
-<main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg">
-    <div class="container-fluid py-4">
-        <div class="row">
-            <div class="col-12">
-                <div class="card mb-4">
-                    <div class="card-header pb-0">
-                        <h6>Election Committee Management</h6>
-                    </div>
-                    <div class="card-body px-0 pt-0 pb-2">
-                        <div class="table-responsive p-0">
-                            <table class="table align-items-center mb-0">
-                                <thead>
-                                    <tr>
-                                        <th class="text-left text-secondary text-xxs font-weight-bolder opacity-7 ps-3">Name</th>
-                                        <th class="text-left text-secondary text-xxs font-weight-bolder opacity-7 ps-3">Role</th>
-                                        <th class="text-left text-secondary text-xxs font-weight-bolder opacity-7 ps-3">Actions</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <!-- Add your committee data here -->
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
+<div>
+    <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg">
+        <div class="container-fluid py-4">
+            <div class="row">
+          <div class="col-12">
+              <div class="card mb-4 mt-4">
+                  <div class="card-header py-4 px-3">
+                      <div class="d-flex flex-row justify-content-between">
+                          <div>
+                              <h5 class="mb-0">Election Committee</h5>
+                          </div>
+                          <a href="#" class="btn bg-gradient-primary btn-sm mb-0" type="button">View More</a>
+                      </div>
+                  </div>
+                  <div class="card-body px-0 pt-0 pb-2">
+                      <div class="table-responsive p-0">
+                          <table class="table align-items-center mb-0">
+                              <thead>
+                                  <tr>
+                                      <th class="text-left text-secondary text-xxs font-weight-bolder opacity-7 ps-3">
+                                          Election
+                                      </th>
+                                      <th class="text-left text-secondary text-xxs font-weight-bolder opacity-7 ps-3">
+                                          Election Detail
+                                      </th>
+                                      <th class="text-left text-uppercase text-xxs font-weight-bolder opacity-7 ps-3">
+                                          Period
+                                      </th>
+                                      <th class="text-left text-uppercase text-xxs font-weight-bolder opacity-7 ps-3">
+                                          Status
+                                      </th>
+                                      <th class="text-left text-uppercase text-xxs font-weight-bolder opacity-7 ps-3">
+                                          Action
+                                      </th>
+                                  </tr>
+                              </thead>
+                              <tbody>
+                                  <tr>
+                                      <td class="ps-3">
+                                          <p class="text-xs font-weight-bold mb-0">Election</p>
+                                      </td>
+                                      <td class="ps-3">
+                                          <p class="text-xs font-weight-bold mb-0">Election Detail</p>
+                                      </td>
+                                      <td class="ps-3">
+                                          <p class="text-xs font-weight-bold mb-0">28 Jun 2024 - 28 July 2024</p>
+                                      </td>
+                                      <td class="ps-3">
+                                          <span class="text-xs font-weight-bold mb-0">End</span>
+                                      </td>
+                                      <td class="ps-3">
+                                          <a href="#" class="me-3" data-bs-toggle="tooltip" data-bs-original-title="Edit user">
+                                              <i class="fas fa-user-edit text-secondary"></i>
+                                          </a>
+                                          <span>
+                                              <i class="cursor-pointer fas fa-trash text-secondary"></i>
+                                          </span>
+                                      </td>
+                                  </tr>
+                              </tbody>
+                          </table>
+                      </div>
+                  </div>
+              </div>
+          </div>
         </div>
-    </div>
-</main>
-@endsection 
+    </main>
+</div> 
