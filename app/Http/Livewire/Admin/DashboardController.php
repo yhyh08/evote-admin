1a08f2f6d211bd21f17fd8c891eead8f4a50a7f3
yhyh08 <yihhuei@marslab.com.my>
2024-12-21 15:59:57 +0800

    add dashboard data is real

--- app/Http/Livewire/Admin/DashboardController.php
+++ app/Http/Livewire/Admin/DashboardController.php
@@ -4,14 +4,25 @@ namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
 use App\Models\Election;
+use App\Models\Nomination;
+use App\Models\VotingEligibility;
+use App\Models\Organization;
 
 class DashboardController extends Component
 {
     public $elections;
+    public $electionCount;
+    public $nominationCount;
+    public $votersCount;
+    public $organizationCount;
 
     public function mount()
     {
         $this->elections = Election::all(['election_topic', 'description', 'start_date', 'end_date', 'status']);
+        $this->electionCount = Election::count();
+        $this->nominationCount = Nomination::count();
+        $this->votersCount = VotingEligibility::count();
+        $this->organizationCount = Organization::where('is_active', true)->count();
     }
 
     public function render()
--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -10,8 +10,7 @@
                   <div class="numbers">
                     <p class="text-sm mb-0 text-capitalize font-weight-bold">Election</p>
                     <h5 class="font-weight-bolder mb-0">
-                      2,000
-                      {{-- <span class="text-success text-sm font-weight-bolder">+55%</span> --}}
+                      {{ $electionCount }}
                     </h5>
                   </div>
                 </div>
@@ -32,8 +31,7 @@
                   <div class="numbers">
                     <p class="text-sm mb-0 text-capitalize font-weight-bold">Nomination</p>
                     <h5 class="font-weight-bolder mb-0">
-                      2,000
-                      {{-- <span class="text-success text-sm font-weight-bolder">+3%</span> --}}
+                      {{ $nominationCount }}
                     </h5>
                   </div>
                 </div>
@@ -52,10 +50,9 @@
               <div class="row">
                 <div class="col-8">
                   <div class="numbers">
-                    <p class="text-sm mb-0 text-capitalize font-weight-bold">Total Users</p>
+                    <p class="text-sm mb-0 text-capitalize font-weight-bold">Organization</p>
                     <h5 class="font-weight-bolder mb-0">
-                      2,000
-                      {{-- <span class="text-danger text-sm font-weight-bolder">-2%</span> --}}
+                      {{ $organizationCount }}
                     </h5>
                   </div>
                 </div>
@@ -68,10 +65,31 @@
             </div>
           </div>
         </div>
+      <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+          <div class="card">
+            <div class="card-body p-3">
+              <div class="row">
+                <div class="col-8">
+                  <div class="numbers">
+                    <p class="text-sm mb-0 text-capitalize font-weight-bold">Total Voters</p>
+                    <h5 class="font-weight-bolder mb-0">
+                      {{ $votersCount }}
+                    </h5>
+                  </div>
+                </div>
+                <div class="col-4 text-end">
+                  <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                    <i class="ni ni-badge text-lg opacity-10" aria-hidden="true"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-            <div class="row">
-                <div class="col-12">
-                    <div class="card mb-4 mt-4">
+      <div class="row">
+        <div class="col-12">
+          <div class="card mb-4 mt-4">
                         <div class="card-header py-4 px-3">
                             <div class="d-flex flex-row justify-content-between">
                                 <div>
