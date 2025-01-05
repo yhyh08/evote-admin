01b2ee015637bc9fe221ecb6d791bf2a2c0f5886
yhyh08 <yihhuei@marslab.com.my>
2025-01-05 18:45:09 +0800

    add view more result

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -90,13 +90,17 @@
       <div class="row">
         <div class="col-12">
           <div class="card mb-4 mt-4">
-                        <div class="card-header py-4 px-3">
+          <div class="card-header py-4 px-3">
                             <div class="d-flex flex-row justify-content-between">
                                 <div>
                                     <h5 class="mb-0">Election Results</h5>
                                 </div>
+                                <a href="{{ route('result') }}" class="btn bg-gradient-primary btn-sm mb-0" type="button">
+                                    <i class="fa-solid fa-file-lines me-2 btn-add"></i>View More
+                                </a>
                             </div>
                         </div>
+                       
                         <div class="card-body px-0 pt-0 pb-2">
                             <div class="table-responsive p-0">
                                 <table class="table align-items-center mb-0">
